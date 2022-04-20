1f333c2790e84ce6be6f90994ac12ab129a27256
현영우 <ieeh0122@naver.com>
2022-04-20 09:48:16 +0900

    C프로그래밍기초 8주차 과제1
    숫자 세기

--- HW8/hw8현영우201611868.c
+++ HW8/hw8현영우201611868.c
@@ -2,16 +2,16 @@
 
 int main(void) 
 {
-	int number = 0;
-	int count1 = 0;
-	int count2 = 0;
-	int count3 = 0;
+	int number = 0; //입력받을 정수
+	int count1 = 0; //0~9 의 숫자를 카운트하는 변수
+	int count2 = 0; //10~19 의 숫자를 카운트하는 변수
+	int count3 = 0; //20~29 의 숫자를 카운트하는 변수
 
 	printf("0~29사이의 정수를 입력 받아 10단위로 개수를 세고,개수만큼 *로 표현\n입력 종료(0~29사이 이외의 정수)\n");
 	while (1) {
 		printf("수 : ");
 		scanf_s("%d", &number);
-		if (number >= 0 && number <= 9) {
+		if (number >= 0 && number <= 9) {    //0~9 or 10~19 or 20~29 일떄를 확인하여 각각의 변수를 +1 시킴
 			count1++;
 		}
 		else if (number >= 10 && number <= 19) {
@@ -25,7 +25,7 @@ int main(void)
 		}
 	}
 	printf("0 - 9 : ");
-	for (int i = 0; i < count1; i++) {
+	for (int i = 0; i < count1; i++) {  // 카운트 되었던 변수만큼 *를 출력시킴
 		printf("*");
 	}
 	printf("\n10 - 19 : ");
