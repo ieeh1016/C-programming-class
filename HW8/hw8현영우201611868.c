b4167fdf8e7a680751f20c9115e99b102be59375
현영우 <ieeh0122@naver.com>
2022-04-20 09:38:48 +0900

    C프로그래밍 기초 8주차 과제1
    숫자 개수 세기

---
+++ HW8/hw8현영우201611868.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	int number = 0;
+	int count1 = 0;
+	int count2 = 0;
+	int count3 = 0;
+
+	printf("0~29사이의 정수를 입력 받아 10단위로 개수를 세고,개수만큼 *로 표현\n입력 종료(0~29사이 이외의 정수)\n");
+	while (1) {
+		printf("수 : ");
+		scanf_s("%d", &number);
+		if (number >= 0 && number <= 9) {
+			count1++;
+		}
+		else if (number >= 10 && number <= 19) {
+			count2++;
+		}
+		else if (number >= 20 && number <= 29) {
+			count3++;
+		}
+		else {
+			break;
+		}
+	}
+	printf("0 - 9 : ");
+	for (int i = 0; i < count1; i++) {
+		printf("*");
+	}
+	printf("\n10 - 19 : ");
+	for (int j = 0; j < count2; j++) {
+		printf("*");
+	}
+	printf("\n20 - 29 : ");
+	for (int k = 0; k < count3; k++) {
+		printf("*");
+	}
+
+	
+
+	return 0;
+
+}
+/*
+	---완성---
+	간단한 반복문 예제였기 때문에 쉽게 과제를 해결할 수 있었습니다.
+	수업시간에 배웠던 내용을 잘 활용했기 때문인것같습니다.
+	다음과제까지 열심히 하겠습니다. 감사합니다.
+*/
